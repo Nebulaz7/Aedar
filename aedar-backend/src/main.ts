addf11f79180604a27752514461c1257dc91d846
Ademola Aderemi <158827677+cridiv@users.noreply.github.com>
2025-12-29 18:49:06 +0100

    Update main.ts

--- aedar-backend/src/main.ts
+++ aedar-backend/src/main.ts
@@ -8,7 +8,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://aedar.onrender.com',
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'https://aedar.onrender.com',
+        'http://localhost:3000',
+      ];
+      
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error('Not allowed by CORS'), false);
+    },
     credentials: true,
   });
 
